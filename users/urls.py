1cf9e6c822c0ce54f2b0d48e256d07d45f0dafe5
Hampus Vretinger <brozgamers@hotmail.com>
2024-12-20 17:48:56 +0000

    Fix some small errors

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, FreelancerProfileViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+# Create a router for the viewsets
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
-router.register(r'freelancer_profiles', FreelancerProfileViewSet)
+router.register(r'freelancer-profiles', FreelancerProfileViewSet)
 
+# Define the urlpatterns
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),  # Includes the viewsets from the router
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -10,8 +10,3 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 class FreelancerProfileViewSet(viewsets.ModelViewSet):
     queryset = FreelancerProfile.objects.all()
     serializer_class = FreelancerProfileSerializer
-
-urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
