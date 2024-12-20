4cd901084300ce8ee766f539b5d1f2eb06e9b17e
Hampus Vretinger <brozgamers@hotmail.com>
2024-12-20 16:51:12 +0000

    Register models in Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CustomUser, FreelancerProfile
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(FreelancerProfile)
